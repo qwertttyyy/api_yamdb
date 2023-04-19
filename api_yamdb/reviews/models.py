df11c800e98b35c8cc877f00be215f3e2f9d49cf
qwertttyyy <zelmisha2@gmail.com>
2023-04-19 10:14:03 +0300

    resolved conflicts

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -85,6 +85,44 @@ class User(AbstractUser):
         return self.role == self.USER
 
 
+class Genres(models.Model):
+    """Описывает модель для хранения групп жанров."""
+
+    # потом перенести все числа в константы
+    name = models.CharField(max_length=265)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def __str__(self):
+        return self.name[:15]
+
+
+class Categories(models.Model):
+    """Описывает модель для хранения групп категорий."""
+
+    name = models.CharField(max_length=265)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def __str__(self):
+        return self.name[:15]
+
+
+class Titles(models.Model):
+    """Описывает модель для хранения групп произведений."""
+
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    description = models.TextField()
+    genre = models.ForeignKey(
+        Genres, related_name='titles', on_delete=models.SET_NULL, null=True
+    )
+    category = models.ForeignKey(
+        Categories, related_name='titles', on_delete=models.SET_NULL, null=True
+    )
+
+    def __str__(self):
+        return self.name[:15]
+
+
 class Reviews(models.Model):
     text = models.TextField(null=False)
     author = models.ForeignKey(
@@ -93,7 +131,7 @@ class Reviews(models.Model):
         related_name='reviews'
     )
     title = models.ForeignKey(
-        Title,  # моделька для titles еще не написана, доработать как увижу
+        Titles,  # моделька для titles еще не написана, доработать как увижу
         on_delete=models.CASCADE,
         related_name='reviews'
     )
@@ -111,14 +149,14 @@ class Reviews(models.Model):
 
 
 class Comments(models.Model):
-    models.TextField(null=False)
+    text = models.TextField(null=False)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='comments'
     )
     title = models.ForeignKey(
-        Title,  # моделька для titles еще не написана, доработать как увижу
+        Titles,  # моделька для titles еще не написана, доработать как увижу
         on_delete=models.CASCADE,
         related_name='reviews'
     )
