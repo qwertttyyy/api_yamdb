db1e710f39398e1d79b312819c2b6f90d59c5ee0
Смурыгин Степан <st1teec@googlemail.com>
2023-04-20 00:21:25 +0700

    добавил урлы для комментов и отзывов

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,10 +1,22 @@
-from api.views import UserViewSet, get_token, signup
+from api.views import (
+    UserViewSet, get_token, signup, ReviewViewSet, CommentViewSet
+)
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 router_v1 = DefaultRouter()
 
 router_v1.register(r'users', UserViewSet)
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
