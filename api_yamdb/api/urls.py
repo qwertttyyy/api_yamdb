7c207f7db00dbfa6cc005718b9f5df0cddb2bb70
Elizaveta Kochergina <kochergin625@gmail.com>
2023-04-19 18:32:00 +0600

    after review fixed urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,13 @@
-from api.views import UserViewSet, get_token
+from api.views import UserViewSet, get_token, signup
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
+router_v1 = DefaultRouter()
 
-router.register(r'users', UserViewSet)
+router_v1.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
     path('v1/auth/token/', get_token),
+    path('v1/auth/signup/', signup),
 ]
