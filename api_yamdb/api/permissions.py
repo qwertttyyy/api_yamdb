44679094d32e902ba60dcc5a3151d4eddb1c6330
Смурыгин Степан <st1teec@googlemail.com>
2023-04-20 00:19:02 +0700

    добавил новый пермишен

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -60,3 +60,23 @@ class ReadOnly(BasePermission):
         del view
         del obj
         return request.method in SAFE_METHODS
+
+
+class IsAdminModeratorAuthorOrReadOnly(BasePermission):
+    """
+    права доступа если пользователь автор, модератор или админ
+    для остальных только чтение
+    """
+    def has_permission(self, request, view):
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in SAFE_METHODS
+            or obj.author == request.user
+            or request.user.is_moderator
+            or request.user.is_admin
+        )
