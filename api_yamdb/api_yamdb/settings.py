cee94c1d4e6946f5edc6dfe57334958ab41b5521
qwertttyyy <zelmisha2@gmail.com>
2023-04-20 19:00:20 +0300

    added core app with command writecsv, updated model Title

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -24,10 +24,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'django_filters',
     'rest_framework',
     'rest_framework_simplejwt',
+
     'api',
+    'core',
     'reviews',
 ]
 # fmt: on
