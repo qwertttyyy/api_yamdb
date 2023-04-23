f933d609ebfc260e5d4341f37b38fefe62001b7a
Смурыгин Степан <st1teec@googlemail.com>
2023-04-23 23:40:46 +0700

    добавил дополнительное поле поиска для отзыва

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -213,7 +213,11 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAdminModeratorAuthorOrReadOnly,)
 
     def get_queryset(self):
-        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
+        review = get_object_or_404(
+            Review,
+            id=self.kwargs.get('review_id'),
+            title__id=self.kwargs.get('title_id'),
+        )
         return review.comments.all()
 
     def perform_create(self, serializer):
